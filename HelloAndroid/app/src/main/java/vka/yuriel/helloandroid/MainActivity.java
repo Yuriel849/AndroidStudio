85ff6c81c2ac1d98b319d59eaf83dd39119c634f
Yuriel849 <yuriel849sp@gmail.com>
2020-08-02 14:32:44 +0200

    Button events

--- HelloAndroid/app/src/main/java/vka/yuriel/helloandroid/MainActivity.java
+++ HelloAndroid/app/src/main/java/vka/yuriel/helloandroid/MainActivity.java
@@ -3,10 +3,10 @@ package vka.yuriel.helloandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     /*
         App creates MainActivity activity.
         onCreate() executed when activity is first created.
@@ -27,7 +27,18 @@ public class MainActivity extends AppCompatActivity {
 
         // findViewById(ID) returns reference to GUI element with the given ID.
         TextView view = findViewById(R.id.text_output);
-        view.setText("This text has been changed in the Java file.");
+        view.setText("Text changed during loading.");
         // Set the text displayed in the text view.
+    }
+
+    /*
+        Button events
+            Layout defines which method to call from Java source code (i.e. method name)
+            Java source code is where the method is implemented.
+                Method signature must be "public void methodName(View view)"
+     */
+    public void onClickSetText(View view) {
+        TextView textView = findViewById(R.id.text_output);
+        textView.setText("Button clicked.");
     }
 }
