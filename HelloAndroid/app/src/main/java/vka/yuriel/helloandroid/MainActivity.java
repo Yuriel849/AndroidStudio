b3cfee14db11bc78189b47d74f32581f46824ccd
Yuriel849 <yuriel849sp@gmail.com>
2020-08-02 13:51:20 +0200

    Using string resources

--- HelloAndroid/app/src/main/java/vka/yuriel/helloandroid/MainActivity.java
+++ HelloAndroid/app/src/main/java/vka/yuriel/helloandroid/MainActivity.java
@@ -3,6 +3,7 @@ package vka.yuriel.helloandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
             resource identifiers as static constants. Resources are what are in the "res" directory,
             ex) "drawable", "layout", "strings"...
         Strings are kept in a string resource file.
+            XML resource file is used for strings for localization (different languages).
         References to GUI elements are kept and strings are displayed in Java files.
      */
 
@@ -22,5 +24,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // findViewById(ID) returns reference to GUI element with the given ID.
+        TextView view = findViewById(R.id.text_output);
+        view.setText("This text has been changed in the Java file.");
+        // Set the text displayed in the text view.
     }
 }
